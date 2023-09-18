eed835db181d8a1dd53b555997cae72a9b36695e
Jordan Bottrell <bottrell@umich.edu>
2023-09-18 11:10:29 -0400

    added ability to programmatically add different experience positions

--- Pages/Partials/_Experienceitem.cshtml.cs
+++ Pages/Partials/_Experienceitem.cshtml.cs
@@ -4,10 +4,12 @@ namespace PersonalWebsite.Pages.Partials;
 
 public class ExperienceItemModel : PageModel
 {
-    public string date { get; set; } = "September 2023 - Current";
-    public string content { get; set; } = "content goes here";
-    void OnGet()
+    public string? date { get; set; } = "September 2023 - Current";
+    public string? content { get; set; } = "content goes here";
+    public List<string>? skills { get; set; } = new List<string> { "HTML", "CSS", "JavaScript" };
+    public ExperienceItemModel(string? d, string? c)
     {
-        //TODO - implement this
+        this.date = d;
+        this.content = c;
     }
 }
